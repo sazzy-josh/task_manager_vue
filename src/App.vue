9f2e5075ffda39a3e6544ec492deae6d25b8c003
sazzy-josh <idahosajoshua61@gmail.com>
2022-12-14 22:19:09 +0100

    created a local json-server

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
-<script setup>
-
+<script>
+  
 
 </script>
 
 <template>
+  <div class="body">
+   <h2>Hello there</h2>
+   <router-view />
+  </div>
+  
 
 </template>
 
 <style>
+*{
+    font-family: 'Source Sans Pro', sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    
+}
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import { Home , About} from "../views";
 
+
+const routes = [
+    {
+        path : '/',
+        component : Home
+    } ,
+    {
+        path : '/about',
+        component : About
+    }
+]
+
+const router = createRouter({
+      history : createWebHistory() ,
+      routes
+})
+
+
+export default router
